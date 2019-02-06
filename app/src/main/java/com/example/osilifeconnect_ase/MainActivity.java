90e2113a09826340eff747f443d3f3db5c092a11
collygon <collygon@gmail.com>
2019-02-06 13:00:42 -0600

    Generated login method. TODO still required.

--- app/src/main/java/com/example/osilifeconnect_ase/MainActivity.java
+++ app/src/main/java/com/example/osilifeconnect_ase/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.osilifeconnect_ase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,27 +13,17 @@ public class MainActivity extends AppCompatActivity {
 
     private Button loginButton = findViewById(R.id.loginButton);
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void loginMethod(View view){
+        if(usernameTextField.getText().toString().equals("Username"))
+            if(passwordTextField.getText().toString().equals("Password"))
+                //TODO: Implement page change activity.
+        return;
+    }
 
 }
