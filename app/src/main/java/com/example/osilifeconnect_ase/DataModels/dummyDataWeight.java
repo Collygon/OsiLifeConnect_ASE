2ea3e0fb844c0231e948e040ee97b3627c98cdb2
cullen-lowe <cullen.lowe@yahoo.com>
2019-03-20 19:30:36 -0500

    adding dummy dataWeight updating weightActivity, activity_weight, and recycler_view_item --Cullen

---
+++ app/src/main/java/com/example/osilifeconnect_ase/DataModels/dummyDataWeight.java
@@ -0,0 +1,39 @@
+package com.example.osilifeconnect_ase.DataModels;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class dummyDataWeight extends dummyDataSuperClass {
+    private double weightLbs;
+    private double weightKgs;
+    private Date date;
+
+    public dummyDataWeight(int MRN){
+        super(MRN);
+
+    }
+    public void setWeightLbs(double weight){
+        this.weightLbs=weight;
+    }
+
+    public double getWeightLbs(){
+        return this.weightLbs;
+    }
+
+    public void setWeightKgs(double weight){
+        this.weightKgs=weight * .45359237;
+    }
+
+    public double getWeightKgs(){return this.weightKgs;}
+
+    public Date getDate(){
+        this.date = new Date();
+        return this.date;
+    }
+
+    public String getDateString() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yy hh:mm");
+        return dateFormat.format(date);
+    }
+
+}
